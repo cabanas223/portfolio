91c4c0154c845c07783a10114129e65d8f0c7b03
cabanas223 <84342715+cabanas223@users.noreply.github.com>
2021-05-17 15:43:19 -0300

    Add files via upload

---
+++ html.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<style type="text/css">
+		body {
+			margin:0px;
+		}
+		.topo {
+			height:50px;
+			background-color:#8E8D8A;
+			width:100%;
+			color:white;
+			font-style:italic;
+			line-height:50px;
+			text-align:center;
+		}
+
+		.topo h1 {
+			margin:0px;
+		}
+
+		.box_form {
+			width:100%;
+			max-width:500px;
+			background-color:#C0392B;
+			margin:auto; 
+ 
+		}
+	</style>
+</head>
+<body>
+
+	<div class="topo">
+		
+		<h1> Preencha os dados</h1>
+	</div>
+<div class="box_form">
+	<div class="form">
+	<form>
+		Nome:<br>
+		<input type="text" name="nome"><br>
+		Email:<br>
+		<input type="text" name="email"><br>
+		Senha:<br>
+		<input type="password" name="senha"><br>
+		<br>
+		<input type="submit" name="enviar">
+	</div>
+
+
+
+	</form>	
+
+
+
+
+
+</div>
+
+
+
+
+
+
+</body>
+</html>
